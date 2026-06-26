23297fc9c309526c62617fd99f657341b425abe8
poorvivaidya <poorvivaidya47@gmail.com>
2026-06-26 13:27:27 +0530

    Fix WhiteNoise static storage

--- event_cert/settings.py
+++ event_cert/settings.py
@@ -102,7 +102,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    "whitenoise.storage.CompressedStaticFilesStorage"
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
