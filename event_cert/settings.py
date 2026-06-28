30e7f5f59dc625ca121367cfa147a47bb4283e3d
poorvivaidya <poorvivaidya47@gmail.com>
2026-06-28 11:29:03 +0530

    Did few changes in Storages

--- event_cert/settings.py
+++ event_cert/settings.py
@@ -105,6 +105,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": MEDIA_ROOT,
+        },
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
